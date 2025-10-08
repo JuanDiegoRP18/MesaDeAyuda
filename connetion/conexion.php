9200f1db2f89e50f3e2c2417bec27a0f0d3e6f00
Juan Diego <rodriguezpabonjuandiego@gmail.com>
2025-10-08 13:09:37 -0500

    Update conexion.php

--- connetion/conexion.php
+++ connetion/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "servicedesk";
+$host = "bwdywwoziyiuddmklza4-mysql.services.clever-cloud.com";
+$user = "ux4pbioyrnpczygi";
+$password = "ux4pbioyrnpczygi";
+$database = "bwdywwoziyiuddmklza4";
 
 $conn = new mysqli($host, $user, $password, $database);
 
