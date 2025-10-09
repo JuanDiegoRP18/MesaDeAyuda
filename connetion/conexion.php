d868c1eb7d67f69fe8743e9774dbefaa1fdb8f44
Bren <jhaderjha26@gmail.com>
2025-10-09 14:06:49 -0500

    Solucion coneccion bd

--- connetion/conexion.php
+++ connetion/conexion.php
@@ -1,7 +1,7 @@
 <?php
 $host = "bwdywwoziyiuddmklza4-mysql.services.clever-cloud.com";
 $user = "ux4pbioyrnpczygi";
-$password = "ux4pbioyrnpczygi";
+$password = "eHYuYj8tYxOLhmRPut5n";
 $database = "bwdywwoziyiuddmklza4";
 
 $conn = new mysqli($host, $user, $password, $database);
